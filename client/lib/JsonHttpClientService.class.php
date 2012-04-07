e327ca97423b5993c588f74091d233030ec3f8c2
DracoBlue <JanS@DracoBlue.de>
2012-04-07 20:38:45 +0200

    return null if no response is given

--- client/lib/JsonHttpClientService.class.php
+++ client/lib/JsonHttpClientService.class.php
@@ -7,7 +7,7 @@ class JsonHttpClientService extends HttpClientService
         $raw_response = parent::rawRequest($method, $url, $options, $values, $auth);
         
         $response = @json_decode($raw_response, true);
-        if ($response === null && $raw_response !== 'null')
+        if ($response === null && $raw_response !== 'null' && $raw_response !== '')
         {
             throw new Exception('Invalid json response: ' . $raw_response);
         }
