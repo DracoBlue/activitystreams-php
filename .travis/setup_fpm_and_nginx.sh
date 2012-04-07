a0643e9edf694c5214c8a8ab0229960b3fbc765c
DracoBlue <JanS@DracoBlue.de>
2012-04-07 23:10:10 +0200

    restart fpm after mysql installation in travis

--- .travis/setup_fpm_and_nginx.sh
+++ .travis/setup_fpm_and_nginx.sh
@@ -4,5 +4,6 @@ apt-get install php5-fpm php5-mysql
 cd `dirname $0`
 cp nginx.conf /etc/nginx/nginx.conf
 /etc/init.d/nginx restart
+/etc/init.d/php5-fpm restart
 cp server.config.php ../server/local.config.php
 cp client.config.php ../client/local.config.php
