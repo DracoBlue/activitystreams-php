e5538cae2d3f1469d77ef2aaf12ec20895419838
DracoBlue <JanS@DracoBlue.de>
2012-04-07 23:19:39 +0200

    removed dash, because it does not work on travis

--- .travis/server.config.php
+++ .travis/server.config.php
@@ -1,7 +1,7 @@
 <?php
 Config::setValues(
     array(
-        'database.dsn' => 'mysql:dbname=naith;host=127.0.0.1;charset=UTF-8',
+        'database.dsn' => 'mysql:dbname=naith;host=127.0.0.1;charset=UTF8',
         "database.user" => "root",
         "database.password" => "",
         "endpoint_base_url" => "http://localhost:8080" . dirname(__FILE__) . "/pub/index.php/"
