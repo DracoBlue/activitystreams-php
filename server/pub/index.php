6dd01718fb1b7e8c7c4da87d9312daa8dd434fcc
DracoBlue <JanS@DracoBlue.de>
2012-05-08 23:53:28 +0200

    Properly read data for PUT requests

--- server/pub/index.php
+++ server/pub/index.php
@@ -12,6 +12,12 @@ require_once(dirname(dirname(__FILE__)) . '/lib/Services.class.php');
 require_once(dirname(dirname(__FILE__)) . '/global.config.php');
 require_once(dirname(dirname(__FILE__)) . '/local.config.php');
 
+if ($_SERVER['REQUEST_METHOD'] === 'PUT')
+{
+    $put_data = file_get_contents('php://input');
+    parse_str($put_data, $_REQUEST);
+}
+
 $dispatcher_service = Services::get('HttpResourceDispatcher');
 $response = $dispatcher_service->dispatchRequest($_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO'], $_REQUEST);
 
