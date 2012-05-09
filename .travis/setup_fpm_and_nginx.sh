5584402ecd991c62f29ee580fb88bc327fe33fe9
DracoBlue <JanS@DracoBlue.de>
2012-05-09 08:04:57 +0200

    added --fix-missing for travis apt-get install, because update takes to long on travis

--- .travis/setup_fpm_and_nginx.sh
+++ .travis/setup_fpm_and_nginx.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-apt-get update
-apt-get install nginx
-apt-get install php5-fpm php5-mysql
+apt-get install nginx --fix-missing
+apt-get install php5-fpm php5-mysql --fix-missing
 cd `dirname $0`
 cp nginx.conf /etc/nginx/nginx.conf
 echo "Restart NGINX"
