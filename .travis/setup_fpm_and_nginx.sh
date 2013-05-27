9f52d7d050b6bd51240d1adfeca19b7c8e041eff
DracoBlue <JanS@DracoBlue.de>
2013-05-27 22:37:23 +0200

    output php -i on travis

--- .travis/setup_fpm_and_nginx.sh
+++ .travis/setup_fpm_and_nginx.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install nginx --fix-missing
 apt-get install php5-fpm php5-mysql php5-xdebug --fix-missing
-cat /etc/php5/conf.d/xdebug.ini
+php -i
 cd `dirname $0`
 cp nginx.conf /etc/nginx/nginx.conf
 echo "Restart NGINX"
